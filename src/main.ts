0521ab8a6c993786e9ad8d6450388e4b96c2e4e2
joaocansi <whitshs2003@gmail.com>
2024-12-12 13:26:06 -0300

    fix: change port to 3000

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(CustomValidationPipe);
   app.useGlobalFilters(new AppErrorFilter());
   app.useGlobalFilters(new CustomErrorValidationFilter());
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
